2b9f47b1cd9234192b3ddb376cc46f239b1506a2
devid.phin <phinsopheacheadavid@gmail.com>
2026-06-04 16:35:38 +0700

    Show File

---
+++ src/ShowFile.java
@@ -0,0 +1,31 @@
+/* Display a text file.
+
+To use this program, specify the name of the file that
+you want to see. For example, to see a file called TEST.TXT,
+use the following command line.
+
+      > java ShowFile TEST.TXT <-!
+
+*/
+
+import java.io.*;
+
+class ShowFile {
+    public static void main(String[] args) throws IOException {
+        int i;
+        FileInputStream fin;
+        try {
+            fin = new FileInputStream("D:/year2/a4y2/MyThis.java");
+        } catch(FileNotFoundException exc) {
+            System.out.println("File Not Found");
+            return;
+        }
+        // read bytes until EOF is encountered
+        do {
+            i = fin.read();
+            if(i != -1)
+                System.out.print((char) i);
+        } while(i != -1);
+        fin.close();
+    }
+}
